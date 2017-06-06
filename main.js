44edfd8d9678e36d6b8718d05cab1c48c36e0545
mosh7890 <mosh7890@gmail.com>
2017-06-06 11:06:58 +0300

    no idea

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var post = [];
 var id = 1;
 
-//var comment = '<form><h3>Comment</h3><div class="form-group"><input type="text" id="username" class="form-control" placeholder="Post Username"><input type="text" id="post-comment" class="form-control" placeholder="Post Comment"></div><button class="btn btn-primary add-comment" type="button">Comment</button></form>'
+var comment = '<form><h3>Comment</h3><div class="form-group"><input type="text" id="username" class="form-control" placeholder="Post Username"><input type="text" id="post-comment" class="form-control" placeholder="Post Comment"></div><button class="btn btn-primary add-comment" type="button">Comment</button></form>'
 
 
 var newPost = function (id, text) {
@@ -11,7 +11,7 @@ var newPost = function (id, text) {
 var updatePost = function () {
     $(".posts").empty();
     for (i = 0; i < post.length; i++) {
-        $(".posts").append('<br><p class="post" data-id="' + post[i].id + '">' + post[i].text + '<br><button class="btn btn-primary remove-post" type="button">Remove Post</button>');
+        $(".posts").append('<br><p class="post" data-id="' + post[i].id + '">' + post[i].text + '<br><button class="btn btn-primary remove-post" type="button">Remove Post</button>' + comment);
     }
 }
 
@@ -24,8 +24,8 @@ $(".add-post").click(function () {
 
 $(".posts").on("click", "button", function () {
     post.splice($(this.closest("p")), 1);
-    // $(this.closest("form").remove());
-    $(this.closest("p")).remove();  
+    $(this.closest("p")).remove();
+    $(".posts").find("form").remove();
 })
 
 
